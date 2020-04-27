23f403e2bfb56a9a23815e0191f0c1943fa16549
Bruno Gasparetto <brunogasparetto@gmail.com>
2020-04-26 23:46:44 -0400

    Adição dos métodos das classes PermissionAssetVO e PermissionVO

--- fluig.d.ts
+++ fluig.d.ts
@@ -1218,8 +1218,30 @@ declare namespace com.fluig.sdk.api.oauth {
 }
 
 declare namespace com.fluig.sdk.api.permission {
-    class PermissionAssetVO {}
-    class PermissionVO {}
+    class PermissionAssetVO {
+        getId(): number;
+        getCode(): string;
+        getDescription(): string;
+        getPageCode(): string;
+        getTypeCode(): string;
+        setId(id: number): void;
+        setCode(code: string): void;
+        setDescription(description: string): void;
+        setPageCode(pageCode: string): void;
+        setTypeCode(typeCode: string): void;
+    }
+    class PermissionVO {
+        getCategory(): string;
+        getCategoryCode(): string;
+        getIsSelected(): boolean;
+        getPermission(): string;
+        getPermissionDescription(): string;
+        setCategory(category: string): void;
+        setCategoryCode(categoryCode: string): void;
+        setIsSelected(isSelected: boolean): void;
+        setPermission(permission: string): void;
+        setPermissionDescription(permissionDescription: string): void;
+    }
 }
 
 declare namespace com.fluig.sdk.api.search {
