34a4ffd2d0fb278285da54cbffb8c622500913aa
Bruno Gasparetto <brunogasparetto@gmail.com>
2020-04-26 23:41:05 -0400

    Adição dos métodos do AlertVO

--- fluig.d.ts
+++ fluig.d.ts
@@ -523,7 +523,34 @@ declare namespace com.fluig.sdk.api.alert {
     class AlertObjectVO {}
     class AlertSenderVO {}
     class AlertUserVO {}
-    class AlertVO {}
+    class AlertVO {
+        getActions(): java.util.List<com.fluig.sdk.api.alert.AlertActionVO>;
+        getCanRemove(): boolean;
+        getCreationDate(): Date;
+        getCreationDateTime(): number;
+        getCurrentDate(): Date;
+        getEvent(): com.fluig.sdk.api.alert.AlertObjectVO;
+        getId(): number;
+        getObject(): com.fluig.sdk.api.alert.AlertObjectVO;
+        getPlace(): com.fluig.sdk.api.alert.AlertObjectVO;
+        getPriority(): string;
+        getRead(): boolean;
+        getReceiver(): com.fluig.sdk.api.alert.AlertUserVO;
+        getSenders(): java.util.List<com.fluig.sdk.api.alert.AlertSenderVO>;
+        setActions(actions: java.util.List<com.fluig.sdk.api.alert.AlertActionVO>): void;
+        setCanRemove(canRemove: boolean): void;
+        setCreationDate(creationDate: Date): void;
+        setCreationDateTime(creationDateTime: number): void;
+        setCurrentDate(currentDate: Date): void;
+        setEvent(event: com.fluig.sdk.api.alert.AlertEventVO): void;
+        setId(id: number): void;
+        setObject(object: com.fluig.sdk.api.alert.AlertObjectVO): void;
+        setPlace(place: com.fluig.sdk.api.alert.AlertObjectVO): void;
+        setPriority(priority: string): void;
+        setRead(read: boolean): void;
+        setReceiver(receiver: com.fluig.sdk.api.alert.AlertUserVO): void;
+        setSenders(senders: java.util.List<com.fluig.sdk.api.alert.AlertSenderVO>): void;
+    }
 }
 
 declare namespace com.fluig.sdk.api.authorizeclient {
