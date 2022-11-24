33a17e3c5194c3d09fcb6fdc4801799ea01f6248
Bruno Gasparetto <brunogasparetto@gmail.com>
2022-11-23 23:58:57 -0400

    Adiciona os tipos utilizados na criação de mecanismo customizado

--- fluig.d.ts
+++ fluig.d.ts
@@ -2543,6 +2543,40 @@ declare namespace log {
     declare function fatal(message: string): void;
 };
 
+interface WorkflowProcessPK {
+    companyId: number,
+    processInstanceId: number
+}
+
+interface WorkflowProcess {
+    workflowProcessPK: WorkflowProcessPK,
+    processId: string,
+    version: number,
+    requesterId: string,
+    active: boolean,
+    attachmentSeqId: number,
+    sourceProcess: number,
+    sourceThreadSequence: number,
+    UUID: string
+}
+
+interface colleaguePK {
+    companyId: number,
+    colleagueId: string
+}
+
+interface Colleague {
+    colleaguePK: colleaguePK,
+    userTenantId: number,
+    colleagueName: string,
+    mail: string,
+    login: string,
+    passwd: string,
+    active: boolean ,
+    adminUser: boolean ,
+    groupId: string
+}
+
 /**
  * Configuração para uma requisição efetuada pelo método WCMAPI.Create
  */
