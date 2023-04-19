829691423e5e707af1f6570f4cd0d5558fd3b700
Bruno Gasparetto <brunogasparetto@gmail.com>
2023-04-18 23:54:15 -0400

    Adiciona o tipo java.lang.String e alguns métodos
    Após alguns problemas tentando executar Regex em strings, tanto para
    verificar uma expressão quanto para efetuar substituições, percebi que
    há muitos problemas entre o tipo string e o java.lang.String.

--- src/java/lang.d.ts
+++ src/java/lang.d.ts
@@ -5,4 +5,155 @@ declare namespace java.lang {
          */
         toString(): string;
     }
+
+    declare class String {
+        constructor();
+        constructor(original: string);
+
+        /**
+         * Pega o char da posição indicada.
+         *
+         * Importante: embora o método deva devolver um char o Fluig
+         * trata como um número (provavelmente o código ASCII do char).
+         *
+         * Se precisar da letra utilize o método substring indicando índice
+         * inicial e final.
+         *
+         * @example
+         * var str = new java.lang.String("texto");
+         * str.substring(1, 2); // Retornará "e"
+         */
+        charAt(index: number): number;
+
+        /**
+         * Compara duas strings
+         *
+         * Retorna 0 se as strings forem iguais, menor que zero se essa string
+         * for menor do que a outra string ou maior que zero se essa string for
+         * maior do que a outra.
+         */
+        compareTo(anotherString: string): number;
+
+        /**
+         * Compara duas strings ignorando as diferenças de maiúscula e minúscula
+         *
+         * Retorna 0 se as strings forem iguais, menor que zero se essa string
+         * for menor do que a outra string ou maior que zero se essa string for
+         * maior do que a outra.
+         */
+        compareToIgnoreCase(anotherString: string): number;
+
+        /**
+         * Retorna verdadeiro se essa string contém a string informada
+         */
+        contains(substring: string): boolean;
+
+        /**
+         * Retorna verdadeiro se essa string termina com a string informada
+         */
+        endsWith(suffix: string): boolean;
+
+        /**
+         * Retorna verdadeiro se essa string começa com a string informada
+         */
+        startsWith(prefix: string): boolean;
+
+        /**
+         * Retorna verdadeiro se ambas strings forem iguais ignorando case
+         */
+        equalsIgnoreCase(anotherString: string): boolean;
+
+        /**
+         * Retorna o índice da primeira ocorrência da string informada
+         */
+        indexOf(str: string): number;
+
+        /**
+         * Retorna o índice da primeira ocorrência da string informada a partir do índice indicado
+         */
+        indexOf(str: string, fromIndex: number): number;
+
+        /**
+         * Retorna o índice da última ocorrência da string informada
+         */
+        lastIndexOf(str: string): number;
+
+        /**
+         * Retorna o índice da última ocorrência da string informada a partir do índice indicado
+         */
+        lastIndexOf(str: string, fromIndex: number): number;
+
+        /**
+         * Retorna a quantidade de caracteres da string
+         */
+        length(): number;
+
+        /**
+         * Retorna verdadeiro se a string satisfaz a Expressão Regular
+         */
+        matches(regex: string): boolean;
+
+        /**
+         * Substitui nessa string todos os trechos que satisfaçam a string target
+         *
+         * Importante: esse método não aceita Expressão Regular.
+         *
+         * @param target Texto a procurar
+         * @param replacement Texto a substituir
+         */
+        replace(target: string, replacement: string): String;
+
+        /**
+         * Substitui nessa string todos os trechos que satisfaçam a string de Expressão Regular
+         *
+         * @param regex String de Expressão Regular
+         * @param replacement Texto a substituir
+         */
+        replaceAll(regex: string, replacement: string): String
+
+        /**
+         * Divide a string em arrays satisfazendo a Expressão Regular fornecida
+         *
+         * @param regex String de Expressão Regular
+         */
+        split(regex: string): String[];
+
+        /**
+         * Divide a string em arrays satisfazendo a Expressão Regular fornecida
+         *
+         * @param regex String de Expressão Regular
+         * @param limit Número máximo de partes a dividir a string
+         */
+        split(regex: string, limit: number): String[];
+
+        /**
+         * Retorna uma substring iniciando no índice indicado até o final da string
+         *
+         * @param beginIndex Índice inicial, começando em 0
+         */
+        substring(beginIndex: number): String;
+
+        /**
+         * Retorna uma substring iniciando no índice indicado até o índice final
+         *
+         * @param beginIndex Índice inicial, começando em 0
+         * @param endIndex Índice final, começando em 0
+         */
+        substring(beginIndex: number, endIndex: number): String;
+
+        /**
+         * Converte a string para letras minúsculas
+         */
+        toLowerCase(): String;
+
+        /**
+         * Converte a string para letras maiúsculas
+         */
+        toUpperCase(): String;
+
+        /**
+         * Remove espaços em branco do início e fim da string
+         */
+        trim(): String;
+    }
 }
