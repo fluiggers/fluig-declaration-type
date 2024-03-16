57733598793289de9add6744943d6d05320afd9b
Bruno Gasparetto <bruno.gasparetto@ms.sebrae.com.br>
2024-03-16 12:30:07 -0400

    Adiciona a classe Integer de Java

--- src/java/lang.d.ts
+++ src/java/lang.d.ts
@@ -177,4 +177,16 @@ declare namespace java.lang {
          */
         digit(c: number, radix: number): number;
     }
+
+    declare class Integer extends Object {
+        /**
+         * Converte a String em Integer
+         */
+        valueOf(value: String): Integer;
+
+        /**
+         * Converte a String em int
+         */
+        parseInt(value: String): number;
+    }
 }
